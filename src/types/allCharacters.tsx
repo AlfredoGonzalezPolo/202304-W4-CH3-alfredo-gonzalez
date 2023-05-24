06bbdd1ca333517860c3653e11a294c66731063b
Alfredo González Polo <fredigonzapolo@hotmail.com>
2023-05-24 19:50:14 +0200

    Create all characters structure

---
+++ src/types/allCharacters.tsx
@@ -0,0 +1,6 @@
+import { Adviser } from "./character";
+import { Fighter } from "./character";
+import { Squires } from "./character";
+import { Kings } from "./character";
+
+export type AllCharacters = Adviser | Fighter | Squires | Kings;
